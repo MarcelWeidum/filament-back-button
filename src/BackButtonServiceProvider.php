891092c342ce9167822139554261d788349127e6
MarcelWeidum <marcelweidum@live.nl>
2026-04-12 09:35:22 +0200

    Cleanup skeleton

--- src/BackButtonServiceProvider.php
+++ src/BackButtonServiceProvider.php
@@ -32,12 +32,9 @@ final class BackButtonServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package->name(self::$name)
-            ->hasCommands($this->getCommands())
             ->hasInstallCommand(function (InstallCommand $command) {
                 $command
                     ->publishConfigFile()
-                    ->publishMigrations()
-                    ->askToRunMigrations()
                     ->askToStarRepoOnGitHub('marcelweidum/filament-back-button');
             });
 
@@ -45,10 +42,6 @@ final class BackButtonServiceProvider extends PackageServiceProvider
 
         if (file_exists($package->basePath("/../config/{$configFileName}.php"))) {
             $package->hasConfigFile();
-        }
-
-        if (file_exists($package->basePath('/../database/migrations'))) {
-            $package->hasMigrations($this->getMigrations());
         }
 
         if (file_exists($package->basePath('/../resources/lang'))) {
@@ -62,93 +55,5 @@ final class BackButtonServiceProvider extends PackageServiceProvider
 
     public function packageRegistered(): void {}
 
-    public function packageBooted(): void
-    {
-        // Asset Registration
-        FilamentAsset::register(
-            $this->getAssets(),
-            $this->getAssetPackageName()
-        );
-
-        FilamentAsset::registerScriptData(
-            $this->getScriptData(),
-            $this->getAssetPackageName()
-        );
-
-        // Icon Registration
-        FilamentIcon::register($this->getIcons());
-
-        // Handle Stubs
-        if (app()->runningInConsole()) {
-            foreach (app(Filesystem::class)->files(__DIR__.'/../stubs/') as $file) {
-                $this->publishes([
-                    $file->getRealPath() => base_path("stubs/filament-back-button/{$file->getFilename()}"),
-                ], 'filament-back-button-stubs');
-            }
-        }
-
-        // Testing
-        Testable::mixin(new TestsBackButton());
-    }
-
-    protected function getAssetPackageName(): ?string
-    {
-        return 'marcelweidum/filament-back-button';
-    }
-
-    /**
-     * @return array<Asset>
-     */
-    protected function getAssets(): array
-    {
-        return [
-            // AlpineComponent::make('filament-back-button', __DIR__ . '/../resources/dist/components/filament-back-button.js'),
-            // Css::make('filament-back-button-styles', __DIR__ . '/../resources/dist/filament-back-button.css'),
-            // Js::make('filament-back-button-scripts', __DIR__ . '/../resources/dist/filament-back-button.js'),
-        ];
-    }
-
-    /**
-     * @return array<class-string>
-     */
-    protected function getCommands(): array
-    {
-        return [
-            BackButtonCommand::class,
-        ];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getIcons(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getRoutes(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string, mixed>
-     */
-    protected function getScriptData(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getMigrations(): array
-    {
-        return [
-            'create_filament-back-button_table',
-        ];
-    }
+    public function packageBooted(): void {}
 }
