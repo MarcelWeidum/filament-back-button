aaf3a43fe1ca3b1b6b973ae4f58b99751dc6aec5
MarcelWeidum <marcelweidum@live.nl>
2026-04-19 16:14:50 +0200

    Style Filament back button component

--- resources/views/back-button.blade.php
+++ resources/views/back-button.blade.php
@@ -1,1 +1,21 @@
-<button>Back</button>
+<style>
+    .fi-back-button {
+        display: inline-flex;
+        margin-inline-end: 0.75rem;
+        vertical-align: middle;
+    }
+
+    .fi-back-button + .fi-header-heading {
+        display: inline-block;
+        vertical-align: middle;
+    }
+</style>
+
+<x-filament::button
+    color="gray"
+    onclick="window.history.back()"
+    class="fi-back-button items-center justify-center text-sm font-medium text-gray-500 transition hover:text-gray-700 focus:outline-none dark:text-gray-400 dark:hover:text-gray-200"
+>
+    <span aria-hidden="true">&larr;</span>
+    <span class="sr-only">Back</span>
+</x-filament::button>
